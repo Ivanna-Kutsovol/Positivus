30aacb3c6ed89f4ce4096c50e74c72c06578f64f
Ivanna-Kutsovol <st6815720@stud.duikt.edu.ua>
2025-02-04 23:18:25 +0200

    continue developing Testimonials block

--- src/app/testimonials/testimonials.tsx
+++ src/app/testimonials/testimonials.tsx
@@ -1,5 +1,12 @@
+'use client'
+
 import React from "react";
 import stl from "./testimonials.module.scss";
+import {Swiper , SwiperSlide} from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
 
 const Testimonials = () => {
     return(
@@ -10,12 +17,88 @@ const Testimonials = () => {
                     <p className={stl.main__descriptionText}>Hear from Our Satisfied Clients: Read Our Testimonials to Learn More about Our Digital Marketing Services</p>
                 </section>
             </section>
-            <section className={stl.container}>
-                <div>
-                John Smith
-                Marketing Director at XYZ Corp
+        <Swiper className={stl.swiper}
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView = "auto"
+        navigation = {{nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev"}}
+        loop={true}
+        effect="fade"
+        centeredSlides={true}
+        pagination={{ clickable: true }}
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+            <SwiperSlide style={{width: "50%"}} className={stl.swiper__container}>
+                <div className={stl.swiper__info}>
+                    <div className={stl.swiper__text}>
+                        "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. 
+                        The team is professional, responsive, and truly cares about the success of our business. 
+                        We highly recommend Positivus to any company looking to grow their online presence."
+                    </div>
+                    John Smith
+                    Marketing Director at XYZ Corp
                 </div>
-            </section>
+            </SwiperSlide>
+
+            <SwiperSlide style={{width: "50%"}} className={stl.swiper__container}>
+                <div className={stl.swiper__info}>
+                    <div className={stl.swiper__text}>
+                        "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. 
+                        The team is professional, responsive, and truly cares about the success of our business. 
+                        We highly recommend Positivus to any company looking to grow their online presence."
+                    </div>
+                    John Smith
+                    Marketing Director at XYZ Corp
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide style={{width: "50%"}} className={stl.swiper__container}>
+                <div className={stl.swiper__info}>
+                    <div className={stl.swiper__text}>
+                        "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. 
+                        The team is professional, responsive, and truly cares about the success of our business. 
+                        We highly recommend Positivus to any company looking to grow their online presence."
+                    </div>
+                    John Smith
+                    Marketing Director at XYZ Corp
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide style={{width: "50%"}} className={stl.swiper__container}>
+                <div className={stl.swiper__info}>
+                    <div className={stl.swiper__text}>
+                        "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. 
+                        The team is professional, responsive, and truly cares about the success of our business. 
+                        We highly recommend Positivus to any company looking to grow their online presence."
+                    </div>
+                    John Smith
+                    Marketing Director at XYZ Corp
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide style={{width: "50%"}} className={stl.swiper__container}>
+                <div className={stl.swiper__info}>
+                    <div className={stl.swiper__text}>
+                        "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. 
+                        The team is professional, responsive, and truly cares about the success of our business. 
+                        We highly recommend Positivus to any company looking to grow their online presence."
+                    </div>
+                    John Smith
+                    Marketing Director at XYZ Corp
+                </div>
+            </SwiperSlide>
+        </Swiper>
+        <div className={stl.swiper__buttonContainer}>
+            <div className="swiper-button-next"/>
+            <div className="swiper-button-prev"/>
+        </div>
         </section>
         
     )

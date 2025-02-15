1dcf5ebc6a248a047c5ea97e69d4b4e6c5772995
Ivanna-Kutsovol <st6815720@stud.duikt.edu.ua>
2025-02-15 22:30:52 +0200

    add block Search Engine

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./hero";
+import SearchEngine from "./searchEngine/searchEngine";
 
 export default function Services(){
     return(
         <div>
             <Hero/>
+            <SearchEngine/>
         </div>
     )
 
---
+++ src/app/services/searchEngine/searchEngine.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import stl from "./searchEngine.module.scss";
+import Image from "next/image";
+import Star from "../../../../public/aboutUs/illustration.webp";
+import Button from "@/components/UI/button/button";
+import starMobile from "../../../../public/aboutUs/ourJourneyIllustration/starMobile.webp";
+
+const searchEngine = () =>{
+    return(
+        <section className={stl.main}>
+            <div className={stl.main__start}>
+                <Image className={stl.main__star} src={Star} width={35} height={35} alt="star"/>
+                <Image className={stl.main__starMobile} src={starMobile} width={18} height={18} alt="starMobile"/>
+                <div>
+                    <h3 className={stl.main__topicMobile}>FEATURED</h3>
+                    <h3 className={stl.main__topic}>Search engine</h3>
+                    <h3 className={stl.main__topic}>optimization</h3>
+                </div>
+            </div>
+            
+            <div className={stl.main__content}>
+                <p>SEO is the process of improving your website’s visibility on search engines like Google. 
+                    By optimizing your content and site structure, 
+                    we help your business rank higher in search results, driving more organic traffic and potential customers.</p>
+                <Button type="submit" className={stl.main__button}>Boost My Rankings</Button>
+            </div>
+        </section>
+    )
+}
+
+export default searchEngine;
